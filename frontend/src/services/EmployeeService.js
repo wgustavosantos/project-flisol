e7c577c4dd9226cfb9cfd651543d529b6a2505f6
Wenderson Santos <guto15santos@gmail.com>
2022-04-20 13:16:53 -0300

    rota redirecionada para localhost

--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_BASE_REST_API_URL = 'https://flisolufpa.herokuapp.com/api/v1/employees';
+const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8080/api/v1/employees';
 
 class EmployeeService{
 
